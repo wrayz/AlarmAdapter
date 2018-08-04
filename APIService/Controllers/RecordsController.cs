790f0125ee9c22e91dd7d83b0e415314d73e8f28
Ray <ruizhouwu@gmail.com>
2018-08-04 19:45:53 +0800

    重構：數據設備記錄異常、正常

--- APIService/Controllers/RecordsController.cs
+++ APIService/Controllers/RecordsController.cs
@@ -1,5 +1,6 @@
 ﻿using APIService.Model;
 using BusinessLogic;
+using ModelLibrary;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,7 @@ namespace APIService.Controllers
     /// </summary>
     public class RecordsController : ApiController
     {
+        //商業邏輯
         private Record_BLL _bll = new Record_BLL();
 
         /// <summary>
@@ -30,23 +32,113 @@ namespace APIService.Controllers
                     return Content(HttpStatusCode.Forbidden, new APIResponse("License key 無效，請檢查License Key"));
                 }
 
+                //轉換資料
                 var content = Request.Content.ReadAsStringAsync().Result;
                 var body = JsonConvert.DeserializeObject<Dictionary<string, string>>(content);
+                var list = _bll.DataConvert(body);
 
-                //log時間
-                var time = DateTime.Now;
                 //記錄檔
-                File.AppendAllText("C:/EyesFree/DataLog.txt", string.Format("{0}, Log: {1}\n", time.ToString(), content));
+                FileDataLog(content);
 
-                //紀錄資料
-                _bll.ModifyRecords(body);
+                //監控參數取得
+                var limit = _bll.GetLimit();
 
-                return Ok();
+                //回應
+                var response = "";
+
+                //資料儲存
+                foreach (var record in list)
+                {
+                    if (string.IsNullOrEmpty(record.DEVICE_ID)) break;
+
+                    //設備資料取得
+                    var device = _bll.GetDevice(record);
+                    //沒有設備編號
+                    if (string.IsNullOrEmpty(device.DEVICE_SN)) break;
+
+                    //設備編號
+                    record.DEVICE_SN = device.DEVICE_SN;
+                    //數據記錄新增
+                    _bll.AddRecord(record);
+
+                    //異常
+                    if (((record.RECORD_TEMPERATURE > limit.MAX_TEMPERATURE_VAL || record.RECORD_TEMPERATURE < limit.MIN_TEMPERATURE_VAL) ||
+                    (record.RECORD_HUMIDITY > limit.MAX_HUMIDITY_VAL || record.RECORD_HUMIDITY < limit.MIN_HUMIDITY_VAL)) &&
+                     device.RECORD_STATUS == "N")
+                    {
+                        //紀錄資料
+                        var data = new RecordLog
+                        {
+                            DEVICE_SN = record.DEVICE_SN,
+                            RECORD_TIME = record.RECORD_TIME,
+                            RECORD_TEMPERATURE = record.RECORD_TEMPERATURE,
+                            RECORD_HUMIDITY = record.RECORD_HUMIDITY
+                        };
+
+                        _bll.ModifyRecordLog("Abnormal", data);
+
+                        //設備數據記錄取得
+                        var recordLog = _bll.GetRecordLog(device.DEVICE_SN);
+                        //推送通知
+                        response += PushNotification(EventType.Error, recordLog);
+                    }
+                    //恢復
+                    else if ((record.RECORD_TEMPERATURE <= limit.MAX_TEMPERATURE_VAL && record.RECORD_TEMPERATURE >= limit.MIN_TEMPERATURE_VAL) &&
+                             (record.RECORD_HUMIDITY <= limit.MAX_HUMIDITY_VAL && record.RECORD_HUMIDITY >= limit.MIN_HUMIDITY_VAL) &&
+                             (device.RECORD_STATUS == "E" || device.RECORD_STATUS == "R"))
+                    {
+                        //紀錄資料
+                        var data = new RecordLog
+                        {
+                            DEVICE_SN = record.DEVICE_SN,
+                            RECOVER_TIME = record.RECORD_TIME
+                        };
+
+                        _bll.ModifyRecordLog("Recover", data);
+
+                        //設備數據記錄取得
+                        var recordLog = _bll.GetRecordLog(device.DEVICE_SN);
+                        //推送通知
+                        response += PushNotification(EventType.Recover, recordLog);
+                    }
+                }
+
+                return Content(HttpStatusCode.OK, new APIResponse(response));
             }
             catch (Exception ex)
             {
                 return Content(HttpStatusCode.InternalServerError, new APIResponse(ex.Message));
             }
+        }
+
+        /// <summary>
+        /// 推送通知
+        /// </summary>
+        /// <param name="type">資料動作</param>
+        /// <param name="data">數據記錄資料</param>
+        /// <returns></returns>
+        private string PushNotification(EventType type, RecordLog data)
+        {
+            var result = "";
+            var payload = new RecordPayload("EyesFree", type, data);
+            var pushService = new PushService(payload);
+
+            result += pushService.PushIM() + "\n";
+            result += pushService.PushDesktop();
+
+            return result;
+        }
+
+        /// <summary>
+        /// 記錄檔
+        /// </summary>
+        /// <param name="content">原始資料</param>
+        private void FileDataLog(string content)
+        {
+            //log時間
+            var time = DateTime.Now;
+            //記錄檔
+            File.AppendAllText("C:/EyesFree/DataLog.txt", string.Format("{0}, Log: {1}\n", time.ToString(), content));
         }
     }
 }
--- BusinessLogic/Record_BLL.cs
+++ BusinessLogic/Record_BLL.cs
@@ -1,54 +1,23 @@
-﻿using BusinessLogic.Event;
-using DataAccess;
+﻿using DataAccess;
 using ModelLibrary;
 using ModelLibrary.Generic;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Globalization;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace BusinessLogic
 {
     /// <summary>
-    /// 數據紀錄資料商業邏輯
+    /// 數據設備記錄商業邏輯
     /// </summary>
-    public class Record_BLL
+    public class Record_BLL : GenericBusinessLogic<RecordLog>
     {
-        private IDataAccess<Record> _dao = GenericDataAccessFactory.CreateInstance<Record>();
-
-        /// <summary>
-        /// IM 伺服器位址
-        /// </summary>
-        private readonly string _url = ConfigurationManager.AppSettings["im"];
-
-        /// <summary>
-        /// 系統名稱
-        /// </summary>
-        private readonly string _system = "EyesFree";
-
-        /// <summary>
-        /// 記錄資料
-        /// </summary>
-        /// <param name="dict">原始資料</param>
-        /// <returns></returns>
-        public void ModifyRecords(Dictionary<string, string> dict)
-        {
-            //資料轉換
-            var data = DataConvert(dict);
-            //紀錄處理
-            ProcessRecord(data);
-        }
-
         /// <summary>
         /// 資料轉換
         /// </summary>
         /// <param name="dict">原始資料</param>
         /// <returns></returns>
-        private IEnumerable<Record> DataConvert(Dictionary<string, string> dict)
+        public IEnumerable<Record> DataConvert(Dictionary<string, string> dict)
         {
             //監控欄位數
             const int COLUMN_NUM = 2;
@@ -97,169 +66,83 @@ namespace BusinessLogic
         }
 
         /// <summary>
-        /// 紀錄處理
-        /// </summary>
-        /// <param name="list">紀錄清單</param>
-        private void ProcessRecord(IEnumerable<Record> list)
-        {
-            //監控參數
-            var limit = GetLimit();
-            var dao = GenericDataAccessFactory.CreateInstance<Device>();
-            var option = new QueryOption();
-
-            foreach (var record in list)
-            {
-                if (string.IsNullOrEmpty(record.DEVICE_ID))
-                    return;
-
-                //設備取得
-                var device = dao.Get(option, new Device { DEVICE_ID = record.DEVICE_ID, DEVICE_TYPE = "D", IS_MONITOR = "Y" });
-                
-                if (!string.IsNullOrEmpty(device.DEVICE_SN))
-                {
-                    record.DEVICE_SN = device.DEVICE_SN;
-                    _dao.Modify("Insert", record);
-
-                    if (((record.RECORD_TEMPERATURE > limit.MAX_TEMPERATURE_VAL || record.RECORD_TEMPERATURE < limit.MIN_TEMPERATURE_VAL) ||
-                        (record.RECORD_HUMIDITY > limit.MAX_HUMIDITY_VAL || record.RECORD_HUMIDITY < limit.MIN_HUMIDITY_VAL)) &&
-                         device.RECORD_STATUS == "N")
-                        AbnormalRecord(record);
-                    else if ((record.RECORD_TEMPERATURE <= limit.MAX_TEMPERATURE_VAL && record.RECORD_TEMPERATURE >= limit.MIN_TEMPERATURE_VAL) &&
-                             (record.RECORD_HUMIDITY <= limit.MAX_HUMIDITY_VAL && record.RECORD_HUMIDITY >= limit.MIN_HUMIDITY_VAL) &&
-                             (device.RECORD_STATUS == "E" || device.RECORD_STATUS == "R"))
-                        RecoverRecord(record);
-                }
-            }
-        }
-
-        /// <summary>
         /// 監控參數取得
         /// </summary>
         /// <returns></returns>
-        private RecordLimit GetLimit()
+        public RecordLimit GetLimit()
         {
             var dao = GenericDataAccessFactory.CreateInstance<RecordLimit>();
             return dao.Get(new QueryOption());
         }
 
         /// <summary>
-        /// 異常紀錄處理
+        /// 設備資料取得
         /// </summary>
-        /// <param name="record"></param>
-        private void AbnormalRecord(Record record)
-        {
-            var dao = GenericDataAccessFactory.CreateInstance<RecordLog>();
-
-            //異常紀錄資料
-            var recordLog = new RecordLog
-            {
-                DEVICE_SN = record.DEVICE_SN,
-                RECORD_TIME = record.RECORD_TIME,
-                RECORD_TEMPERATURE = record.RECORD_TEMPERATURE,
-                RECORD_HUMIDITY = record.RECORD_HUMIDITY
-            };
-
-            dao.Modify("Abnormal", recordLog);
-
-            var logDao = GenericDataAccessFactory.CreateInstance<DeviceRecord>();
-            var log = logDao.Get(new QueryOption(), new DeviceRecord { DEVICE_SN = recordLog.DEVICE_SN });
-            recordLog = dao.Get(new QueryOption { Plan = new QueryPlan { Join = "Payload" } }, new RecordLog { LOG_SN = log.LOG_SN });
-
-            var fields = new List<Field>
-            {
-                new Field("設備名稱", recordLog.DEVICE_INFO.DEVICE_NAME, true),
-                new Field("設備位址", recordLog.DEVICE_INFO.DEVICE_ID, true),
-                new Field("異常數據", string.Format("溫度：{0} 度\n濕度：{1} %", recordLog.RECORD_TEMPERATURE, record.RECORD_HUMIDITY), false),
-                new Field("異常時間", recordLog.RECORD_TIME.Value.ToString(@"MM\/dd\/yyyy HH:mm"), true)
-            };
-
-            var payload = new Payload
-            {
-                LOG_SN = recordLog.LOG_SN,
-                LOG_TYPE = "D",
-                DEVICE_SN = recordLog.DEVICE_SN,
-                SYSTEM_NAME = _system,
-                BUTTON_STATUS = "E",
-                COLOR = "danger",
-                TITLE = "設備異常資訊",
-                GROUP_LIST = recordLog.GROUP_LIST,
-                FIELD_LIST = fields
-            };
-
-            //推送訊息
-            PushMessage(payload);
-        }
-
-        /// <summary>
-        /// 恢復紀錄處理
-        /// </summary>
-        /// <param name="record"></param>
-        private void RecoverRecord(Record record)
-        {
-            var dao = GenericDataAccessFactory.CreateInstance<RecordLog>();
-
-            //恢復紀錄資料
-            var recordLog = new RecordLog
-            {
-                DEVICE_SN = record.DEVICE_SN,
-                RECOVER_TIME = record.RECORD_TIME
-            };
-
-            var logDao = GenericDataAccessFactory.CreateInstance<DeviceRecord>();
-            var log = logDao.Get(new QueryOption(), new DeviceRecord { DEVICE_SN = recordLog.DEVICE_SN });
-
-            dao.Modify("Recover", recordLog);
-
-            recordLog = dao.Get(new QueryOption { Plan = new QueryPlan { Join = "Payload" } }, new RecordLog { LOG_SN = log.LOG_SN });
-
-            var fields = new List<Field>
-            {
-                new Field("設備名稱", recordLog.DEVICE_INFO.DEVICE_NAME, true),
-                new Field("設備位址", recordLog.DEVICE_INFO.DEVICE_ID, true),
-                new Field("現在數據", string.Format("溫度：{0} 度\n濕度：{1} %", record.RECORD_TEMPERATURE, record.RECORD_HUMIDITY), false),
-                new Field("恢復時間", recordLog.RECOVER_TIME.Value.ToString(@"MM\/dd\/yyyy HH:mm"), true),
-                new Field("處理人員", recordLog.USER_INFO.USER_NAME, true)
-            };
-
-            var payload = new Payload
-            {
-                LOG_SN = recordLog.LOG_SN,
-                LOG_TYPE = "D",
-                DEVICE_SN = recordLog.DEVICE_SN,
-                SYSTEM_NAME = _system,
-                BUTTON_STATUS = "N",
-                COLOR = "good",
-                TITLE = "異常設備恢復資訊",
-                GROUP_LIST = recordLog.GROUP_LIST,
-                FIELD_LIST = fields
-            };
-
-            //推送訊息
-            PushMessage(payload);
-        }
-
-        /// <summary>
-        /// 訊息推送
-        /// </summary>
+        /// <param name="record">數據記錄資料</param>
         /// <returns></returns>
-        private async Task<HttpStatusCode> PushMessage(Payload payload)
+        public Device GetDevice(Record record)
         {
-            //http POST推送設定
-            using (var client = new HttpClient())
+            var dao = GenericDataAccessFactory.CreateInstance<Device>();
+            var data = new Device
             {
-                //伺服器位址
-                client.BaseAddress = new Uri(_url);
+                DEVICE_ID = record.DEVICE_ID,
+                DEVICE_TYPE = "D",
+                IS_MONITOR = "Y"
+            };
+            return dao.Get(new QueryOption(), data);
+        }
 
-                //內容
-                var content = new FormUrlEncodedContent(new[]{
-                    new KeyValuePair<string, string>("info", JsonConvert.SerializeObject(payload))
-                });
+        /// <summary>
+        /// 數據記錄資料取得
+        /// </summary>
+        /// <param name="sn">設備編號</param>
+        /// <returns></returns>
+        public RecordLog GetRecordLog(string sn)
+        {
+            var deviceRecord = GetDeviceRecord(sn);
 
-                //post
-                var result = await client.PostAsync("im/eyesFreeLog", content);
+            //查詢條件
+            var option = new QueryOption
+            {
+                Plan = new QueryPlan { Join = "Payload" }
+            };
 
-                return result.StatusCode;
-            }
+            //資料
+            var recordLog = new RecordLog { LOG_SN = deviceRecord.LOG_SN.Value };
+
+            return _dao.Get(option, recordLog);
+        }
+
+        /// <summary>
+        /// 數據設備記錄資料取得
+        /// </summary>
+        /// <param name="sn">設備編號</param>
+        /// <returns></returns>
+        private DeviceRecord GetDeviceRecord(string sn)
+        {
+            var dao = GenericDataAccessFactory.CreateInstance<DeviceRecord>();
+            return dao.Get(new QueryOption(), new DeviceRecord { DEVICE_SN = sn });
+        }
+
+        /// <summary>
+        /// 數據記錄資料新增
+        /// </summary>
+        /// <param name="record">數據記錄</param>
+        public void AddRecord(Record record)
+        {
+            var dao = GenericDataAccessFactory.CreateInstance<Record>();
+            dao.Modify("Insert", record);
+        }
+
+        /// <summary>
+        /// 數據異常紀錄物件處理
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="recordLog"></param>
+        public void ModifyRecordLog(string type, RecordLog recordLog)
+        {
+            var dao = GenericDataAccessFactory.CreateInstance<RecordLog>();
+            dao.Modify(type, recordLog);
         }
     }
 }
