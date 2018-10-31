3e427a249b29c156e5818828af5b23685965774c
Ray <ruizhouwu@gmail.com>
2018-10-31 14:04:18 +0800

    實體黑名單資訊新增

---
+++ ModelLibrary/BlockHole.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ModelLibrary
+{
+    /// <summary>
+    /// 黑名單資訊
+    /// </summary>
+    public class BlockHole
+    {
+        /// <summary>
+        /// 黑名單 IP
+        /// </summary>
+        public string IP_ADDRESS { get; set; }
+
+        /// <summary>
+        /// 黑名單分數
+        /// </summary>
+        public int ABUSE_SCORE { get; set; }
+
+        /// <summary>
+        /// 請求時間
+        /// </summary>
+        public DateTime REQUEST_TIME { get; set; }
+    }
+}
