b5cbadd0c09cc9e5db6f81a9165e4941fb1edcb3
Ray <ruizhouwu@gmail.com>
2018-11-12 19:05:09 +0800

    新增 Cacti API 控制器

---
+++ APIService/Controllers/CactiController.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLogic.Director;
+using BusinessLogic.License;
+using ModelLibrary.Enumerate;
+using System;
+using System.Net;
+using System.Web.Http;
+
+namespace APIService.Controllers
+{
+    public class CactiController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post()
+        {
+            var detector = "Cacti";
+            var logger = NLog.LogManager.GetLogger(detector);
+
+            try
+            {
+                var record = Request.Content.ReadAsStringAsync().Result;
+                logger.Info(record);
+
+                var license = new LicenseBusinessLogic();
+                license.Verify(DateTime.Now);
+
+                var director = new WorkDirector(detector, record, DeviceType.N);
+                director.Execute();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+                return Content(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+    }
+}
