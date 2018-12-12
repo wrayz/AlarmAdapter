ae3f8dfb4990788dccc9038af26a6d0c9f0f32e5
Ray <ruizhouwu@gmail.com>
2018-12-12 11:58:30 +0800

    移除未使用實體：群組設備資料

--- ModelLibrary/Group.cs
+++ ModelLibrary/Group.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace ModelLibrary
+﻿namespace ModelLibrary
 {
     /// <summary>
     /// 群組資料
@@ -16,15 +14,5 @@ namespace ModelLibrary
         /// 群組名稱
         /// </summary>
         public string GROUP_NAME { get; set; }
-
-        /// <summary>
-        /// 群組設備清單
-        /// </summary>
-        public List<GroupDevice> DEVICE_LIST { get; set; }
-
-        /// <summary>
-        /// 群組成員清單
-        /// </summary>
-        public List<GroupMember> MEMBER_LIST { get; set; }
     }
 }
