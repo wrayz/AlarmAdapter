d8b5498caa3495fd5974364a238ba1ec96cf83a4
Ray <ruizhouwu@gmail.com>
2018-12-12 16:20:45 +0800

    建立 攝像機接收 API

---
+++ APIService/Controllers/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using APIService.Director;
+using ModelLibrary.Enumerate;
+using System;
+using System.Web;
+using System.Web.Http;
+
+namespace APIService.Controllers
+{
+    /// <summary>
+    /// 攝像機接收 API
+    /// </summary>
+    public class CameraController : ApiController
+    {
+        public IHttpActionResult POST()
+        {
+            var detector = "Camera";
+            var logger = NLog.LogManager.GetLogger(detector);
+
+            try
+            {
+                var record = Request.Content.ReadAsStringAsync().Result;
+                var sourceIp = GetSourceIP();
+                logger.Info(sourceIp + "|" + record);
+
+                var director = new GenericRecordDirector(detector, record, DeviceType.S, sourceIp);
+                director.Execute();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex);
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 來源 IP 取得
+        /// </summary>
+        /// <returns></returns>
+        private string GetSourceIP()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Current.Request.ServerVariables["HTTP_VIA"]))
+                return HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+
+            return HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+        }
+    }
+}
