aa35505685a81c5ceb48b6772b99c6f10b59ffcc
Ray <ruizhouwu@gmail.com>
2018-12-18 21:44:05 +0800

    預設監控項目取得 新增 BobCacti, Logmaster

--- BusinessLogic/Target_BLL.cs
+++ BusinessLogic/Target_BLL.cs
@@ -55,9 +55,19 @@ namespace BusinessLogic
                 case Detector.Cacti:
                     target = GetCactiTarget(condition);
                     break;
+
                 case Detector.Camera:
-                    target = GetCameraTarget(condition);
+                    target = GetAlwaysTarget(condition);
                     break;
+
+                case Detector.BobCacti:
+                    target = GetBobCactiTarget(condition);
+                    break;
+
+                case Detector.Logmaster:
+                    target = GetAlwaysTarget(condition);
+                    break;
+
                 default:
                     throw new Exception($"無 { detector } 預設監控項目設定");
             }
@@ -66,11 +76,39 @@ namespace BusinessLogic
         }
 
         /// <summary>
+        /// Bob Cacti 預設監控項目資訊取得
+        /// </summary>
+        /// <param name="condition">實體條件</param>
+        /// <returns></returns>
+        private Target GetBobCactiTarget(Target condition)
+        {
+            var data = new Target
+            {
+                DEVICE_SN = condition.DEVICE_SN,
+                TARGET_NAME = condition.TARGET_NAME,
+                OPERATOR_TYPE = "In",
+                TARGET_STATUS = "0",
+                IS_EXCEPTION = "Y",
+                ALARM_CONDITIONS = new List<AlarmCondition>
+                {
+                    new AlarmCondition
+                    {
+                        DEVICE_SN = condition.DEVICE_SN,
+                        TARGET_NAME = condition.TARGET_NAME,
+                        TARGET_VALUE = "Error"
+                    }
+                }
+            };
+
+            return _dao.Modify("Insert", data);
+        }
+
+        /// <summary>
         /// Camera 預設監控項目資訊取得
         /// </summary>
         /// <param name="condition"></param>
         /// <returns></returns>
-        private Target GetCameraTarget(Target condition)
+        private Target GetAlwaysTarget(Target condition)
         {
             var data = new Target
             {
